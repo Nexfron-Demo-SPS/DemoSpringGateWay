bf44eb7cbcde927346add9a7b107500bd60cfb31
Moondy217 <moondaye0217@gmail.com>
2024-11-20 16:00:36 +0900

    feat: gateway

---
+++ src/test/java/com/example/demospringgateway/DemoSpringGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
